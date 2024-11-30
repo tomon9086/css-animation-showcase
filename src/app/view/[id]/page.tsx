459a7c1d6131b0a972c881461204d1034d1d8b29
tomon9086 <tomon9086@gmail.com>
2024-11-30 17:56:54 +0900

    apply dynamic text as css

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { AnimationBaseButton } from '@/components/animation-base/button'
 import { ReadonlyURLSearchParams } from 'next/navigation'
 
 type Props = {
@@ -5,6 +6,45 @@ type Props = {
   searchParams: Promise<ReadonlyURLSearchParams>
 }
 
+const dummyStyle = (id: string) =>
+  new Promise<string>((resolve, reject) => {
+    if (!id) {
+      reject(new Error('empty id'))
+    }
+
+    resolve(`
+      .anim-button {
+        margin: 1em;
+        padding: 0.8em 1.2em;
+        color: #fff;
+        background-color: #35b531;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: transform 0.1s ease-in-out, background-color 0.1s ease-in-out;
+      }
+
+      .anim-button:hover {
+        transform: scale(1.1);
+        background-color: #42e13d;
+      }
+
+      .anim-button:active {
+        transform: rotate(5deg);
+      }
+    `)
+  })
+
 export default async function View(props: Props) {
-  return <div>Hello, {(await props.params).id || 'World'}!</div>
+  const { id } = await props.params
+  const style = await dummyStyle(id)
+
+  return (
+    <div>
+      <div>
+        <style>{style}</style>
+        <AnimationBaseButton className='anim-button'></AnimationBaseButton>
+      </div>
+    </div>
+  )
 }
